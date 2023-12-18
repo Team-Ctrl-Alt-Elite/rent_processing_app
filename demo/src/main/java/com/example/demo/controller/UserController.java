24403ffab546765ff584fcd530a6327ea2fe91b7
Desai <kavish.desai@fiserv.com>
2023-12-18 16:01:55 -0500

    Custom Queries Working YAYgit add controller/UserController.java repository/UserRepository.java

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -1,22 +1,54 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
-//CORS because react runs on a different port so in order to request access the below line is required
+//CORS because React runs on a different port so in order to request access the below line is required
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
+    // The following endpoints basically joins users(landlords specifically) and Contracts table and returns the result in nice format
+    @GetMapping("/landlordContracts")
+    public ResponseEntity<List<Map<String, Object>>> getALlLandlordContracts() {
+        // Call the repository method that executes the custom query
+        List<Object[]> result = userRepository.findAllLandlordContracts();
+
+        // Transform the Object[] result into List<Map<String, Object>>
+        List<Map<String, Object>> transformedResult = new ArrayList<>();
+        for (Object[] row : result) {
+            Map<String, Object> map = new HashMap<>();
+            // Assuming the columns are selected in the query, adjust the keys accordingly
+            map.put("LandlordId", row[0]);
+            map.put("Landlord_first_name", row[1]);
+            map.put("Landlord_last_name", row[2]);
+            map.put("Landlord_username", row[3]);
+            map.put("Landlord_phonenumber", row[7]);
+            map.put("role", row[8]);
+            map.put("contract_id", row[9]);
+            map.put("unit", row[10]);
+            map.put("monthly_rent", row[11]);
+            map.put("lease_starting_date", row[12]);
+            map.put("tenant_id", row[15]);
+            transformedResult.add(map);
+        }
+        return ResponseEntity.ok(transformedResult);
+    }
 
     @GetMapping("/all")
     public List<User> getAllLandlord() {
@@ -52,5 +84,4 @@ public class UserController {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
-    // Other endpoints for CRUD operations if needed
 }
