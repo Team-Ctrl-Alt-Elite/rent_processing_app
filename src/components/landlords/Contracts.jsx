1a96d2f9a7e11a7dcf93f2aee764e818739f4e4e
McNerlin <tiffany.mcnerlin@fiserv.com>
2023-12-27 07:45:25 -0500

    tm-landlord data fixed & filter, sort added to tables

--- src/components/landlords/Contracts.jsx
+++ src/components/landlords/Contracts.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect, useMemo } from "react";
-import { useTable } from "react-table";
+import { useNavigate } from "react-router-dom";
+import { useSortBy, useTable, useFilters } from "react-table";
 import axios from "axios";
 import "../../styles/LDashboard.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSort } from "@fortawesome/free-solid-svg-icons";
 
 /* LANDLORD DASHBOARD FOR CONTRACTS:
 1. Landlord should be able to see the contracts for every tenant on the property
@@ -12,7 +15,9 @@ export default function Contracts({ getChildProps }) {
   const [contracts, setContracts] = useState([]);
   const [selectedContract, setSelectedContract] = useState(null);
   const [contractDetails, setContractDetails] = useState(null);
+  const [filterInput, setFilterInput] = useState("");
   const data = useMemo(() => contracts, [contracts]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -29,12 +34,15 @@ export default function Contracts({ getChildProps }) {
         console.log("Tenant Contracts: ", tenantContracts?.data);
         setContracts(tenantContracts?.data);
       } catch (err) {
+        if (err.response.status === 401) {
+          navigate("/auth/login");
+        }
         console.log("TDashboard Error:", err);
       }
     };
 
     getAllTenantContracts();
-  }, []);
+  }, [navigate]);
 
   console.log(contracts);
 
@@ -50,6 +58,12 @@ export default function Contracts({ getChildProps }) {
       setContractDetails(selectedContractDetails);
     }
   }, [contracts, selectedContract]);
+
+  useEffect(() => {
+    if (contractDetails !== null && contractDetails !== undefined) {
+      getChildProps(contractDetails);
+    }
+  }, [getChildProps, contractDetails]);
 
   const columns = useMemo(
     () => [
@@ -87,46 +101,68 @@ export default function Contracts({ getChildProps }) {
     []
   );
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    setFilter,
+  } = useTable(
+    {
       columns,
       data,
-    });
+    },
+    useFilters,
+    useSortBy
+  );
+
+  const handleFilter = (e) => {
+    const value = e.target.value || undefined;
+    setFilter("payment_date", value);
+    setFilterInput(value);
+  };
 
   const handleContractClick = (contract) => {
     console.log("Selected Contract:", contract);
-    setSelectedContract(contract);
-
-    if (contractDetails) {
-      return getChildProps(
-        <div>
-          <h2>Contract Details</h2>
-          <p>Contract ID: {selectedContract.contract_id}</p>
-          <p>Unit: {selectedContract.unit}</p>
-          <p>Tenant ID: {selectedContract.tenant_id}</p>
-          <p>Tenant First Name: {selectedContract.Tenant_first_name}</p>
-          <p>Tenant Last Name: {selectedContract.Tenant_last_name}</p>
-          <p>Lease Start Date: {selectedContract.lease_starting_date}</p>
-          <p>Lease End Date: {selectedContract.lease_ending_on}</p>
-          <p>Monthly Rent: {selectedContract.monthly_rent}</p>
-          <p>Tenant Email: {selectedContract.Tenant_username}</p>
-          <p>Tenant Phone Number: {selectedContract.phonenumber}</p>
-        </div>
-      );
-    }
+    if (contract !== null && contract !== undefined)
+      setSelectedContract(contract);
   };
 
   return (
     <section className="container">
       {/* <h3>Contracts</h3> */}
       <div>
+        <input
+          type="text"
+          value={filterInput}
+          onChange={handleFilter}
+          placeholder="Search"
+          className="thead-input"
+        />
         <table {...getTableProps()}>
           <thead>
             {headerGroups.map((headerGroup) => (
               <tr {...headerGroup.getHeaderGroupProps()}>
                 {headerGroup.headers.map((column) => (
-                  <th {...column.getHeaderProps()}>
-                    {column.render("Header")}
+                  <th
+                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                    className={
+                      column.isSorted
+                        ? column.isSortedDesc
+                          ? "sort-desc"
+                          : "sort-asc"
+                        : ""
+                    }
+                  >
+                    <div className="th-wrapper">
+                      {column.render("Header")}
+                      <FontAwesomeIcon
+                        icon={faSort}
+                        size="xs"
+                        className="fa-icon"
+                      />
+                    </div>
                   </th>
                 ))}
               </tr>
--- src/components/landlords/LDashboard.jsx
+++ src/components/landlords/LDashboard.jsx
@@ -6,7 +6,7 @@ import "../../styles/LDashboard.css";
 
 export default function LDashboard() {
   const [activeTab, setActiveTab] = useState("units");
-  const [childProps, setChildProps] = useState();
+  const [childProps, setChildProps] = useState(null);
 
   const handleTabClick = (tabName) => {
     setActiveTab(tabName);
@@ -42,7 +42,53 @@ export default function LDashboard() {
             )}
           </div>
           <div className="ldash-right">
-            <div className="ldash-div > div">{childProps && childProps}</div>
+            <div className="ldash-div > div">
+              {childProps && activeTab === "units" && (
+                <div>
+                  <h2>Unit Details</h2>
+                  <p>Unit: {childProps.id}</p>
+                  <p>Bed: {childProps.bed}</p>
+                  <p>Bath: {childProps.bath}</p>
+                  <p>Size: {childProps.size} sq. ft.</p>
+                  <p>Monthly Rent: ${childProps.rent}.00</p>
+                </div>
+              )}
+              {childProps && activeTab === "contracts" && (
+                <div>
+                  <h2>Contract Details</h2>
+                  <p>Contract ID: {childProps.contract_id}</p>
+                  <p>Unit: {childProps.unit}</p>
+                  <p>Tenant ID: {childProps.tenant_id}</p>
+                  <p>Tenant First Name: {childProps.Tenant_first_name}</p>
+                  <p>Tenant Last Name: {childProps.Tenant_last_name}</p>
+                  <p>Lease Start Date: {childProps.lease_starting_date}</p>
+                  <p>Lease End Date: {childProps.lease_ending_on}</p>
+                  <p>Monthly Rent: {childProps.monthly_rent}</p>
+                  <p>Tenant Email: {childProps.Tenant_username}</p>
+                  <p>Tenant Phone Number: {childProps.phonenumber}</p>
+                </div>
+              )}
+              {childProps && activeTab === "rentLogs" && (
+                <div>
+                  <h2>Rent Payment Log</h2>
+                  <p>Payment ID: {childProps.id}</p>
+                  <p>Contract ID: {childProps.contract_id}</p>
+                  <p>Amount Paid: {childProps.amount_paid}</p>
+                  <p>Payment Date: {childProps.payment_date}</p>
+                  <p>Payment Medium: {childProps.payment_medium}</p>
+                  <p>
+                    {childProps.check_number
+                      ? `Check Number: ${childProps.check_number}`
+                      : "Check Number: N/A"}
+                  </p>
+                  <p>
+                    {childProps.online_transaction_number
+                      ? `Online Transaction Number: ${childProps.online_transaction_number}`
+                      : "Online Transaction Number: N/A"}
+                  </p>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </section>
